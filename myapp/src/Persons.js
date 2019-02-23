3058ba5f9e1cf6cb2ae78b6387c6ba770268e435
Mateusz Iwańczuk <iwanczukmateusz@gmail.com>
2019-02-23 10:59:54 +0100

    Conditional output

--- myapp/src/Persons.js
+++ myapp/src/Persons.js
@@ -4,13 +4,27 @@ class Persons extends Component {
     render(){
         const { persons } = this.props;
         const personsList = persons.map(person => {
-            return (
-                <div className="person" key={person.id}>
-                    <div>Name: { person.name } </div> 
-                    <div>Age: { person.age } </div>
-                </div>
-            )
+            if (person.age >= 30) {
+                return (
+                    <div className="person" key={person.id}>
+                        <div>Name: { person.name } </div> 
+                        <div>Age: { person.age } </div>
+                    </div>
+                )
+            } else {
+                return null;
+            }
         })
+
+        // const personsList = persons.map(person => {
+        //     return person.age >= 30 ? (
+        //         <div className="person" key={person.id}>
+        //             <div>Name: { person.name } </div> 
+        //             <div>Age: { person.age } </div>
+        //         </div>
+        //     ) : null;
+        // })
+
         return(
             <div className="personsList">
                 { personsList }
