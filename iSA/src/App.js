e87f5f5954b187b884d9cb9f648ef116f1523267
Mateusz Iwańczuk <iwanczukmateusz@gmail.com>
2019-02-24 12:45:43 +0100

    Task 6 done : flowers

--- iSA/src/App.js
+++ iSA/src/App.js
@@ -1,14 +1,56 @@
-import React from 'react';
-import User from './User';
+import React from 'react'
+import Lily from './Lily'
 
-// ### Ćwiczenie 5 
-// Stwórz w komponencie App komponent User. User powinien przyjmować propsy name, age, occupation. Stwórz komponent dla każdego z nich i wyświetl go tylko w momencie kiedy dany prop jest obecny.
+// ### Ćwiczenie 6 
+// Stwórz w komponencie `<App />` komponent `<Garden />`. `<Garden />` powinien przyjmować następujące propsy:
+//   -  rose
+//   -  lily
+//   -  gardenia
+//   -  sunflower
+//   -  isWinter
 
-const App = (props) => {
+// Wszystkie propsy **oprócz** `isWinter` powinny być obiektem:
+// ```
+// {
+//    name: nazwa_kwiata // string
+//    height: wysokosc // number
+// }
+// ```
+
+// `isWinter` powinien być booleanem
+
+// Dla każdego kwiatowego propsa stwórz komponent. Każdy z tych komponentów powinien przyjmować propsa `flower`, który wyświetli nazwę kwiata i jego wysokość podaną w centymetrach
+
+// `<Garden />` powinien wyświetlać kwiaty tylko w momencie kiedy `isWinter` jest `false`
+
+// ***Zastanów się jakby można zrefaktoryzować kod***
+
+const Garden = (props) => {
+  return props.flowers.map(flower => <Lily name={flower.name} height={flower.height} />)
+}
+
+const flowers = [
+  {
+    name: 'rose',
+    height: 35
+  },
+  {
+    name: 'lily',
+    height: 35
+  },
+  {
+    name: 'gardenia',
+    height: 35
+  },
+  {
+    name: 'sunflower',
+    height: 35
+  }
+]
+
+const App = () => {
   return (
-    <div>
-      <User name="Mateusz" age={24} occupation="student" />
-    </div>
+    <Garden flowers={flowers} isWinter={false} />
   )
 }
 
---
+++ iSA/src/Lily.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Lily = (props) => {
+  return  <span>{`${props.flower.name} ${props.flower.height}cm`}</span>
+}
+
+export default Lily 
