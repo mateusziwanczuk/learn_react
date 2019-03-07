b01866e23568da5a70835c50d44a4c9c2e0cebea
Mateusz Iwańczuk <iwanczukmateusz@gmail.com>
2019-03-07 14:42:48 +0100

    Example of props children. Created new folder for components.

--- react_course/src/App.js
+++ react_course/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Person from './Components/Person';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Person name="Mateusz" age="24" />
+        <Person name="Martyna" age="22"  >Hobby: Travel</Person>
+        <Person name="Janusz" age="65" />
       </div>
     );
   }
