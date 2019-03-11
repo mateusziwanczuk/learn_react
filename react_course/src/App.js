1c816ebe560321c72297d2af7efbe70d6de11fd2
Mateusz Iwańczuk <iwanczukmateusz@gmail.com>
2019-03-11 22:27:16 +0100

    Introducing to state.

--- react_course/src/App.js
+++ react_course/src/App.js
@@ -3,12 +3,19 @@ import Person from './Components/Person';
 import './App.css';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Mateusz', age: 24 },
+      { name: 'Martyna', age: 22 },
+      { name: 'Janusz', age: 65 },
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <Person name="Mateusz" age="24" />
-        <Person name="Martyna" age="22"  >Hobby: Travel</Person>
-        <Person name="Janusz" age="65" />
+        <Person name={ this.state.persons[0].name} age={ this.state.persons[0].age } />
+        <Person name={ this.state.persons[1].name} age={ this.state.persons[1].age } />
+        <Person name={ this.state.persons[2].name} age={ this.state.persons[2].age } />
       </div>
     );
   }
