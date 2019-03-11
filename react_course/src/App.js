ca14d837216fa68a4743776bcff1e82afcfb60aa
Mateusz Iwańczuk <iwanczukmateusz@gmail.com>
2019-03-12 00:21:16 +0100

    Handling dynamic content

--- react_course/src/App.js
+++ react_course/src/App.js
@@ -17,25 +17,28 @@ class App extends Component {
     this.setState({showPersons: !doesShow});
   }
   render() {
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div >
+          <Person 
+            name={ this.state.persons[0].name} 
+            age={ this.state.persons[0].age } />
+          <Person 
+            name={ this.state.persons[1].name} 
+            age={ this.state.persons[1].age } />
+          <Person 
+            name={ this.state.persons[2].name} 
+            age={ this.state.persons[2].age } />
+        </div> 
+      )
+    }
     return (
       <div className="App">
         <button 
           onClick={this.togglePersonsHandler}>Switch name
         </button>
-        { this.state.showPersons ?
-            <div >
-              <Person 
-                name={ this.state.persons[0].name} 
-                age={ this.state.persons[0].age } />
-              <Person 
-                name={ this.state.persons[1].name} 
-                age={ this.state.persons[1].age } />
-              <Person 
-                name={ this.state.persons[2].name} 
-                age={ this.state.persons[2].age } />
-            </div> 
-            : null
-        }
+        { persons }
       </div>
     );
   }
