cb68ef83222b098d5ca82fbb38f3a569f717ec4f
Mateusz Iwańczuk <iwanczukmateusz@gmail.com>
2019-03-17 12:15:40 +0100

    Store data in state, render list of countries.

--- countries_sort/src/App.js
+++ countries_sort/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = {
+    countries: []
+  }
   componentDidMount() {
     fetch('http://localhost:3000/countries.json')
       .then(res => res.json())
-      .then(console.log)
+      .then(countries=> this.setState({countries}))
   }
   render() {
     return (
-      <h1>Hello world</h1>
+      <>
+        <h1>Countries</h1>
+        <ul>
+            {this.state.countries.map(country => (
+                <li>{country.name}</li>
+            ))}
+        </ul>
+      </>
     );
   }
 }
