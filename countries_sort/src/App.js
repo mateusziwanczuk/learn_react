800635041a3b62b7a78ba040344f33d94e81ebee
Mateusz Iwańczuk <iwanczukmateusz@gmail.com>
2019-03-18 20:00:04 +0100

    Filter by country name and region.

--- countries_sort/src/App.js
+++ countries_sort/src/App.js
@@ -6,8 +6,9 @@ class App extends React.Component {
   state = {
     countries: [],
     regions: [],
-    filterCountries: {
-      text: ''
+    filter: {
+      text: '',
+      region: '',
     }
   }
   componentDidMount() {
@@ -15,24 +16,31 @@ class App extends React.Component {
       .then(res => res.json())
       .then(countries => {
         const regions = countries.reduce((acc, country) => {
-            country.region.split(' ').forEach(region => acc.add(region));
+          country.region.split(' ').forEach(region => acc.add(region));
             return acc;
         }, new Set());
         this.setState({ countries, regions: [...regions]});
     });
   }
+  getData() {
+      return this.state.countries.filter(
+          country => {
+              const countryNameLowerCased = country.name.toLowerCase();
+              const textFilterLowerCased = this.state.filter.text.toLowerCase();
+              const countryRegion = country.region;
+              const regionFilter = this.state.filter.region;
+              return countryNameLowerCased.includes(textFilterLowerCased) && countryRegion.includes(regionFilter);
+          }
+      )
+  }
   render() {
-    return (
+      return(
       <>
-        <h1>Countries</h1>
-        <FilterForm onFilterChange={filter => this.setState({filterCountries: filter})} regions={this.state.regions} />
-        <Table countries = {this.state.countries
-          .filter(country => country.name
-          .toLowerCase()
-          .includes(this.state.filterCountries.text))} 
-        />
+          <h1>Countries</h1>
+          <FilterForm onFilterChange={filter => this.setState({filter})} regions={this.state.regions}/>
+          <Table countries={this.getData()}/>
       </>
-    );
+      ) 
   }
 }
 
--- countries_sort/src/Components/FilterForm.js
+++ countries_sort/src/Components/FilterForm.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 class FilterForm extends React.Component {
     state = {
-        text: ''
+        text: '',
+        region: '',
     }
     onInputChange = e => {
         this.setState(
@@ -10,14 +11,20 @@ class FilterForm extends React.Component {
             () => this.props.onFilterChange(this.state)
         )
     }
+    onSelectChange = e => {
+        this.setState(
+            { region: e.target.value},
+            () => this.props.onFilterChange(this.state)
+        )
+    }
     render(){
         return (
             <form>
                 <input type="text" value={this.state.value} onChange={this.onInputChange} />
-                <select>
+                <select onChange={this.onSelectChange}>
                     <option value="">Region</option>
                     {this.props.regions.map(region => (
-                        <option value={region}>{region}</option>
+                        <option key={region} value={region}>{region}</option>
                     ))}
                 </select>
             </form>
