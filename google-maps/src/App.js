5dd01f33f90e283f6135d1898bc3460b7af45c7e
Mateusz Iwańczuk <iwanczukmateusz@gmail.com>
2019-03-21 14:56:53 +0100

    Install google-maps-react package. Sample usage with lazy-loading Google API code.

--- google-maps/src/App.js
+++ google-maps/src/App.js
@@ -1,11 +1,27 @@
+// DOCUMENTATION:
+// https://www.npmjs.com/package/google-maps-react
+
 import React from 'react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello world</h1>
+      <Map google={this.props.google} zoom={14}>
+
+        <Marker onClick={this.onMarkerClick}
+          name={'Current location'} />
+
+        <InfoWindow onClose={this.onInfoWindowClose}>
+          <div>
+            <h1>{this.state.selectedPlace.name}</h1>
+          </div>
+        </InfoWindow>
+      </Map>
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: (YOUR_GOOGLE_API_KEY_GOES_HERE)
+})(App)
