80b6fceae38f165bb6cb3a06fc58f8147182a978
Mateusz Iwańczuk <iwanczukmateusz@gmail.com>
2019-03-26 21:58:21 +0100

    Display user data. Create User components.

---
+++ supermarket_react/src/components/UserPanel/UserData.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+class Nickname extends React.Component {
+    state = {
+        registered: "",
+        nickname: "",
+        email: "",
+        address: "",
+        phone: "",
+        showInput: false,
+    }
+    componentDidMount(){
+        fetch('user.json')
+            .then(response => response.json())
+            .then(value => this.setState({nickname: value.nickname, registered: value.registered }))
+        fetch('user.json')
+            .then(response => response.json())
+            .then(value => this.setState({ email: value.email, address: `${value.address.street} ${value.address.zipcode} ${value.address.city}`, phone: value.phone}))
+    }
+    editUserData = () => {
+        const doInputsShow = this.state.showInput;
+        this.setState( { 
+            showInput: !doInputsShow 
+        });
+        let inputs = document.querySelectorAll(".change__data__container");
+        if (this.state.showInput) {
+            inputs.forEach( input => input.classList.add("unvisible"));
+        } else {
+            inputs.forEach( input => input.classList.remove("unvisible"));
+        }
+    }
+    editNickname = (e) => { this.setState({ nickname: e.target.value }) }
+    editEmail = (e) => { this.setState({ email: e.target.value }) }
+    editAddress = (e) => { this.setState({ address: e.target.value }) }
+    editPhone = (e) => { this.setState({ phone: e.target.value }) }
+
+    render (){
+        return(
+            <>
+                <span>Registered: {this.state.registered}</span>
+                <h2><span role="img" aria-label="user">👤</span> Login: {this.state.nickname}</h2> 
+                    <div className="change__data__container unvisible">
+                        <input type="text" value={this.state.nickname} onChange={this.editNickname}></input><button onClick={this.editUserData}>OK</button>    
+                    </div>
+                <h2><span role="img" aria-label="phone">📞</span> Contact: </h2>
+                    <h4>e-mail: {this.state.email}</h4>
+                        <div className="change__data__container unvisible">
+                            <input type="email" value={this.state.email} onChange={this.editEmail}></input><button onClick={this.editUserData}>OK</button>    
+                        </div>
+                    <h4>address: {this.state.address}</h4>
+                        <div className="change__data__container unvisible">
+                            <input type="text" value={this.state.address} onChange={this.editAddress}></input><button onClick={this.editUserData}>OK</button>    
+                        </div>
+                    <h4>phone: {this.state.phone} </h4>   
+                        <div className="change__data__container unvisible">
+                            <input type="text" value={this.state.phone} onChange={this.editPhone}></input><button onClick={this.editUserData}>OK</button>    
+                        </div>
+                <div>
+                    <div className="button" onClick={this.editUserData}>
+                        <span>Edit Profile</span>
+                    </div>
+                </div>        
+            </>
+
+        )
+    }
+}
+
+export default Nickname;
--- supermarket_react/src/components/UserPanel/UserPanel.js
+++ supermarket_react/src/components/UserPanel/UserPanel.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
+import Avatar from './Avatar';
+import UserData from './UserData';
+
+import './User.css';
 
 class UserPanel extends React.Component {
-    render(){
+    state = {
+        name: "",
+    }
+    componentDidMount(){
+        fetch('user.json')
+            .then(response => response.json())
+            .then(value => this.setState({name: value.name}))
+    }
+    
+    render (){
         return(
-            <h1>User</h1>
+            <div className="user__container">
+                <div className="user__container__left">
+                    <h1 className="sayHello">Hello {this.state.name}!</h1>
+                    <Avatar />
+                    <UserData />
+                </div>
+                <div className="user__container__right">
+                </div>
+            </div>
         )
     }
 }
 
-export default UserPanel
+export default UserPanel;
