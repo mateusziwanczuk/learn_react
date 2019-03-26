7a527d573ffee656d0f07c636257e02f9e5b9e83
Mateusz Iwańczuk <iwanczukmateusz@gmail.com>
2019-03-26 15:14:11 +0100

    Home page styled. Header font, colors, background.

--- supermarket_react/src/components/Home/Home.js
+++ supermarket_react/src/components/Home/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import "./Home.css"
 
 class Home extends React.Component {
     render(){
         return(
-            <h1>Home</h1>
+            <div className="Home__container">
+                <div className="Home__container__header">
+                    <h1>React</h1>
+                    <p>Supermarket</p>
+                </div>
+            </div>
         )
     }
 }
