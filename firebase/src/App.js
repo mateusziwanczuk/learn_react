71b60f0a57eee3593d4d6451717710a79bd70e07
Mateusz Iwańczuk <iwanczukmateusz@gmail.com>
2019-04-09 17:10:41 +0200

    Firebase installed and imported.

--- firebase/src/App.js
+++ firebase/src/App.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react';
+import firebase from 'firebase'
 import './App.css';
+
+const config = {
+  apiKey: "AIzaSyDBsLbZsyfcgRKu9yT5Wj0wez9_G2n600E",
+  authDomain: "fir-app-155b8.firebaseapp.com",
+  databaseURL: "https://fir-app-155b8.firebaseio.com",
+  projectId: "fir-app-155b8",
+  storageBucket: "fir-app-155b8.appspot.com",
+  messagingSenderId: "437448437927"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
