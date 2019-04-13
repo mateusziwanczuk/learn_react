9adb123e11e74f11f2d1dab5df583837350272cf
Mateusz Iwańczuk <iwanczukmateusz@gmail.com>
2019-04-13 12:21:46 +0200

    Adding products to local storage. Show alerts when add or try to multiply.

--- supermarket_react/src/components/Market/MarketProducts.js
+++ supermarket_react/src/components/Market/MarketProducts.js
@@ -12,13 +12,30 @@ const IconCnt = styled.div`
 
 const MarketProducts = props => {
     const {products} = props;
+
     return(
         <div className="market__product__container">
             {products.map(product => (
-            <div className="market__product" key={product.title}>
+            <div className="market__product" key={product.id}>
                 <img src={product.image} alt={product.image} />
                 <div className="market__product__info">
-                    <IconCnt>
+                    { }
+                    <IconCnt onClick={ () => {
+                        if (localStorage.getItem("basketProducts") !== null) {
+                            let basketProducts = JSON.parse(localStorage.getItem("basketProducts"));
+                            if (!basketProducts.some(item => item.id === product.id)) {
+                                basketProducts.push(product);
+                                alert('Product added to your basket!');
+                                localStorage.setItem("basketProducts", JSON.stringify(basketProducts));
+                            } else {
+                                alert('You can change quantity in Basket bookmark.')
+                            }
+                        } else {
+                            const basketProducts = [product];
+                            alert('Product added to your basket!');
+                            localStorage.setItem("basketProducts", JSON.stringify(basketProducts));
+                        }
+                    }}>
 						<Icon icon={shoppingCart} />
 					</IconCnt>
                     <p>{product.title}</p>
@@ -30,4 +47,4 @@ const MarketProducts = props => {
     )
 }
 
-export default MarketProducts; 
+export default MarketProducts;
