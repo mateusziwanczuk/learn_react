37f5f2089deb73a94d4d4a1e3148d01da466fd71
mateuszmatzh <iwanczukmateusz@gmail.com>
2019-04-13 17:13:19 +0200

    Actions increment, decrement

--- redux/src/reducers/counterReducer.js
+++ redux/src/reducers/counterReducer.js
@@ -1,10 +1,17 @@
-export default function counterReducer(state, action) => {
+import {INCREMENT, DECREMENT} from '../actions/counter'
+
+const initialState = 0
+
+export default function counterReducer(state = initialState, action) {
     switch(action.type){
-        case 'INCREMENT': {
+        case INCREMENT: {
             return state + 1
         }
-        case 'DECREMENT': {
+        case DECREMENT: {
             return state -1
+        }
+        default: {
+            return state
         }
     }
 }
