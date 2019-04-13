a6c23ab140dbd4f9cdc8ec0511914c8132c70194
mateuszmatzh <iwanczukmateusz@gmail.com>
2019-04-13 17:55:32 +0200

    Redux devTools on.

--- redux/src/App.js
+++ redux/src/App.js
@@ -10,21 +10,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-
           <p>
             <button onClick={this.props.decreaseCounter}>-</button>
             <span> COUNTER: {this.props.counter} </span>
             <button onClick={this.props.increaseCounter}>+</button>
           </p>
-
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
