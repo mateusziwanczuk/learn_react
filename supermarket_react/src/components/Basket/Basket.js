2bdf073311ee895e51e5a437bfc52825e208aaac
Mateusz Iwańczuk <iwanczukmateusz@gmail.com>
2019-04-26 11:39:21 +0200

    Create basic basket table. Display product list and total price from data of localStorage.

--- supermarket_react/src/components/Basket/Basket.js
+++ supermarket_react/src/components/Basket/Basket.js
@@ -5,19 +5,42 @@ class Basket extends React.Component {
   state = {
 		basketProducts: JSON.parse(localStorage.getItem('basketProducts'))
 	};
+	totalPrice = this.state.basketProducts
+		.map(basketProduct => basketProduct.price)
+		.reduce((a,b) => a + b)
   render() { 
     return ( 
-      <div className="basket__container">
-        <h1>Basket</h1>
-        {localStorage.getItem("basketProducts") !== null ?
-							this.state.basketProducts.map((product, i) => {
-								return (
-									<div key={product.title}>
-											<h5>{product.title}</h5>
-									</div>
-								);
-							}) : null
-						}
+			<div className="basket__container">
+				<h1>Basket</h1>
+				<table>
+					<thead>
+						<tr>
+							<th>Product</th>
+							<th>Quantity</th>
+							<th>Price</th>
+						</tr>
+					</thead>
+					<tbody>
+						{localStorage.getItem("basketProducts") !== null ?
+								this.state.basketProducts.map((product, i) => {
+									return (
+										<tr key={product.title}>
+												<td>{product.title}</td>
+												<td>1</td>
+												<td>$ {product.price}</td>
+										</tr>
+									);
+								}) : null
+							}
+					</tbody>
+					<tfoot>
+						<tr>
+							<td></td>
+							<td><b>Total price</b></td>
+							<td><b>$ {this.totalPrice.toFixed(2)}</b></td>
+						</tr>
+					</tfoot>
+				</table>
       </div>
     );
   }
