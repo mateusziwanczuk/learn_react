f3bc3abe1d2fac6b0a4c631073724e007050c967
mateuszmatzh <iwanczukmateusz@gmail.com>
2019-05-01 13:03:52 +0200

    Get firebase database.

--- isa_firebase/src/Components/Todo.js
+++ isa_firebase/src/Components/Todo.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 class Todo extends React.Component {
+    state = {
+        todo: []
+    }
+
     render() { 
         return (
-            <h1>Todo</h1>
+            <>
+                <h1>Todo</h1>
+            </>
         );
     }
 }
