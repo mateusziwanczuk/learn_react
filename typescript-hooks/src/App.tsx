e32110732e058285e26b6ea9221f9b6b49e309bb
mateuszmatzh <iwanczukmateusz@gmail.com>
2019-07-08 22:49:11 +0200

    Initial commit

---
+++ typescript-hooks/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+        <h1 className="App-header">REACT HOOKS (TypeScript)</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
